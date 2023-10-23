e5c1eee1fcac20decc5ece33efae035edd42b46f
EHTESHAM855 <23f2005532@ds.study.iitm.ac.in>
2023-10-23 12:27:40 +0530

    mobile responsive

--- SRC/JS/hmr.js
+++ SRC/JS/hmr.js
@@ -1,21 +1,26 @@
-// const menuIcon = document.querySelector('.hamburger');
-// const menuItems = document.querySelector('.mobile-menu');
-
-// menuIcon.addEventListener('click',()=> {
-//     menuItems.classList.toggle('show-menu');
-// });
-
-const menuButton = document.querySelector('.hamburger');
-const menu = document.querySelector('.mobile-menu');
+const menuButton = document.querySelector(".hamburger");
+const menu = document.querySelector(".mobile-menu");
+const bar = document.querySelector(".bar");
+const bar_1 = document.querySelector(".bar-1");
+const bar_2 = document.querySelector(".bar-2");
+const bar_3 = document.querySelector(".bar-3");
+const emptspc = document.querySelector('.empty-sp-cont')
 
 menuButton.addEventListener("click", () => {
-    // menuButton.classList.toggle('close')
-    menu.classList.toggle("show-menu");
+  menu.classList.toggle("show-menu");
+  bar.classList.toggle("bar-open");
+  bar_1.classList.toggle("bar-1-open");
+  bar_2.classList.toggle("bar-2-open");
+  bar_3.classList.toggle("bar-3-open");
+  emptspc.classList.toggle('show-menu');
 });
 
 // Close the menu when clicking outside of it
-document.addEventListener("click", (event) => {
-    if (!menu.contains(event.target) && event.target !== menuButton) {
-        menu.classList.remove("show-menu");
-    }
+emptspc.addEventListener("click", ()=>{
+    menu.classList.toggle("show-menu");
+  bar.classList.toggle("bar-open");
+  bar_1.classList.toggle("bar-1-open");
+  bar_2.classList.toggle("bar-2-open");
+  bar_3.classList.toggle("bar-3-open");
+  emptspc.classList.toggle('show-menu');
 });
